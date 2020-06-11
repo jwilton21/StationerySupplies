566e28b734650777bba3e0ccb685593925ec6928
Jasmin Wilton <45803246+jwilton21@users.noreply.github.com>
2020-06-11 19:01:28 +0100

    Added Json database data

---
+++ StationerySupplies.WebSite/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace StationerySupplies.WebSite.Models
+{
+    public class Product
+    {
+        public String Id { get; set; }
+        public String Maker { get; set; }
+        public  String Image { get; set; }
+
+        public String Url { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public int[] Ratings { get; set; }
+
+    }
+}
