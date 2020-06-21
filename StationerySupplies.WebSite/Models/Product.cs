b88b31228bad074aaac7f58e9e65f6d7dce63d0d
Jasmin Wilton <45803246+jwilton21@users.noreply.github.com>
2020-06-21 21:38:25 +0100

    Added json file.

--- StationerySupplies.WebSite/Models/Product.cs
+++ StationerySupplies.WebSite/Models/Product.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Policy;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace StationerySupplies.WebSite.Models
@@ -10,12 +12,18 @@ namespace StationerySupplies.WebSite.Models
     {
         public String Id { get; set; }
         public String Maker { get; set; }
+
+        [JsonPropertyName("img")]
         public  String Image { get; set; }
 
         public String Url { get; set; }
         public String Title { get; set; }
         public String Description { get; set; }
         public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+
 
     }
 }
---
+++ StationerySupplies.WebSite/Services/JsonFileProductService.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using StationerySupplies.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileProductService
+    {   // Constructor
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        // Retrieve Json file so the webhost enviornment knows the path where file comes from.
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {   // Return from path. Combine 3 parts.
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        
+       public IEnumerable<Product> GetProducts()
+        {   // Convert text from json into product.
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {   // Array of products.
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            if (products.First(x => x.Id == productId).Ratings == null)
+            {
+                products.First(x => x.Id == productId).Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+                ratings.Add(rating);
+                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+    }
+
+}
